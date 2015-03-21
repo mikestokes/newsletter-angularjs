46ac8d2c8ee4a49bf33d82d29aa0cbf6566ccc80
Mike <mike.stokes@helpburner.com>
2015-03-22 11:40:37 +1300

    ADD: login to pocket via oauth

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-/**
- * AngularJS Tutorial 1
- * @author Nick Kaye <nick.c.kaye@gmail.com>
- */
 
 /**
  * Main AngularJS Web Application
@@ -18,12 +14,6 @@ app.config(['$routeProvider', function ($routeProvider) {
     // Home
     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
     .when("/redirect", {templateUrl: "partials/home.html", controller: "RedirectCtrl"})
-    // Pages
-    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
-    .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
-    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
-    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
@@ -31,12 +21,16 @@ app.config(['$routeProvider', function ($routeProvider) {
 /**
  * Controls all other Pages
  */
-app.controller('PageCtrl', function ($scope, $location, $http, $log) {
-alert(document.referrer);
-  $scope.signin = function () {  
-    $http.post('https://hotlunch.azure-mobile.net/api/pocket/', { consumer_key:'39239-0b722d6f64189f7ea3440174' }).
+app.controller('PageCtrl', function ($scope, $location, $http, $log, $window) {
+
+  var consumer_key = '39239-0b722d6f64189f7ea3440174';
+
+  $scope.signin = function () {
+    $http.post('https://hotlunch.azure-mobile.net/api/pocket/code', { consumer_key: consumer_key }).
       success(function(data, status, headers, config) {
-        $log.info('Received code: ' + data);
+        $log.info('Received code: ' + JSON.stringify(data));
+        window.localStorage.setItem('consumer_key', consumer_key);
+        window.localStorage.setItem('code', data.code);
         redirect(data.code, data.redirect_uri);
       }).
       error(function(data, status, headers, config) {
@@ -45,15 +39,52 @@ alert(document.referrer);
   };
 
   var redirect = function (code, redirect_uri) {
-    var uri = 'https://getpocket.com/auth/authorize?request_token=' + code + '&redirect_uri=' + redirect_uri;
-    window.location = uri;
+    var loginWindow = $window.open('https://getpocket.com/auth/authorize?request_token=' + code + '&redirect_uri=' + redirect_uri);
+    loginWindow.onbeforeunload = function () {
+      getPocketArticles();
+    };
+  };
+
+  var getPocketArticles = function () {
+
+    $http.post('https://hotlunch.azure-mobile.net/api/pocket/getlist', {
+        consumer_key: consumer_key,
+        access_token: window.localStorage.getItem('access_token'),
+        state: 'all',
+        detailType: 'complete',
+        sort: 'site'//,
+        //tag: 'list',
+        //favorite: '1'
+      }).
+      success(function(data, status, headers, config) {
+        $log.info('Received articles: ' + data);
+
+      }).
+      error(function(data, status, headers, config) {
+        $log.error('Oops error gettings your Pocket articles');
+      });
   };
 
 });
 
-app.controller('RedirectCtrl', function ($scope, $location, $http, $log) {
+app.controller('RedirectCtrl', function ($scope, $location, $http, $log, $window, $timeout) {
 
-  alert('bang');
+  var consumer_key = window.localStorage.getItem('consumer_key'),
+      code = window.localStorage.getItem('code');
+
+  $http.post('https://hotlunch.azure-mobile.net/api/pocket/token', { consumer_key: consumer_key, code: code }).
+      success(function(data, status, headers, config) {
+        $log.info('Received code: ' + JSON.stringify(data));
+        window.localStorage.setItem('access_token', data.access_token);
+        window.localStorage.setItem('username', data.username);
+
+        $timeout(function() {
+          $window.close();
+        }, 1000);
+      }).
+      error(function(data, status, headers, config) {
+        $log.error('Oops error getting token');
+      });
 
 });
 
