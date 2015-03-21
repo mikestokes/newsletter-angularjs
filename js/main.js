13747731bf744fa64691ada6f677b12e117863b1
Mike Stokes <mike@gingerbreadkids.com>
2015-03-22 09:19:00 +1300

    initial commit

--- js/main.js
+++ js/main.js
@@ -17,39 +17,43 @@ app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
     // Home
     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    .when("/redirect", {templateUrl: "partials/home.html", controller: "RedirectCtrl"})
     // Pages
     .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
     .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
     .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
     .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
     .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
-    // Blog
-    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
-    .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
 
 /**
- * Controls the Blog
- */
-app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
-  console.log("Blog Controller reporting for duty.");
-});
-
-/**
  * Controls all other Pages
  */
-app.controller('PageCtrl', function (/* $scope, $location, $http */) {
-  console.log("Page Controller reporting for duty.");
+app.controller('PageCtrl', function ($scope, $location, $http, $log) {
+alert(document.referrer);
+  $scope.signin = function () {  
+    $http.post('https://hotlunch.azure-mobile.net/api/pocket/', { consumer_key:'39239-0b722d6f64189f7ea3440174' }).
+      success(function(data, status, headers, config) {
+        $log.info('Received code: ' + data);
+        redirect(data.code, data.redirect_uri);
+      }).
+      error(function(data, status, headers, config) {
+        $log.error('Oops error getting code');
+      });
+  };
 
-  // Activates the Carousel
-  $('.carousel').carousel({
-    interval: 5000
-  });
+  var redirect = function (code, redirect_uri) {
+    var uri = 'https://getpocket.com/auth/authorize?request_token=' + code + '&redirect_uri=' + redirect_uri;
+    window.location = uri;
+  };
 
-  // Activates Tooltips for Social Links
-  $('.tooltip-social').tooltip({
-    selector: "a[data-toggle=tooltip]"
-  })
 });
+
+app.controller('RedirectCtrl', function ($scope, $location, $http, $log) {
+
+  alert('bang');
+
+});
+
